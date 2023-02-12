b87c83728938c83246d45a0367d7fc3e678d2aca
andreatirelli <ta.tirelliandrea@gmail.com>
2023-02-12 14:49:29 +0100

    moved some logic for the game, in private method to be the code more readable

--- src/BattleController.java
+++ src/BattleController.java
@@ -1,51 +1,61 @@
 import java.util.Scanner;
 
 public class BattleController {
-  private Pokemon userPokemon;
-  private Pokemon computerPokemon;
+    private Pokemon userPokemon;
+    private Pokemon computerPokemon;
+    
 
-  public BattleController(Pokemon userPokemon, Pokemon computerPokemon) {
-    this.userPokemon = userPokemon;
-    this.computerPokemon = computerPokemon;
-  }
-
-  public void start() {
-    Scanner input = new Scanner(System.in);
-
-    while (userPokemon.getCurrentHP() > 0 && computerPokemon.getCurrentHP() > 0) {
-      System.out.println("Your Pokemon: " + userPokemon.getName());
-      System.out.println("HP: " + userPokemon.getCurrentHP() + "/" + userPokemon.getMaxHP());
-      System.out.println("Opponent's Pokemon: " + computerPokemon.getName());
-      System.out.println("HP: " + computerPokemon.getCurrentHP() + "/" + computerPokemon.getMaxHP());
-      System.out.println();
-
-      System.out.println("Choose a move:");
-      for (int i = 0; i < userPokemon.getMoves().size(); i++) {
-        Move move = userPokemon.getMoves().get(i);
-        System.out.println((i + 1) + ": " + move.getName());
-      }
-
-      int moveIndex = input.nextInt() - 1;
-      Move selectedMove = userPokemon.getMoves().get(moveIndex);
-      userPokemon.useMove(selectedMove, computerPokemon);
-
-      System.out.println();
-      System.out.println(userPokemon.getName() + " used " + selectedMove.getName() + "!");
-
-      if (computerPokemon.getCurrentHP() > 0) {
-        Move computerMove = computerPokemon.getMoves().get(0);
-        computerPokemon.useMove(computerMove, userPokemon);
-
-        System.out.println(computerPokemon.getName() + " used " + computerMove.getName() + "!");
-      }
-
-      System.out.println();
+    public BattleController(Pokemon userPokemon, Pokemon computerPokemon) {
+        this.userPokemon = userPokemon;
+        this.computerPokemon = computerPokemon;
     }
 
-    if (userPokemon.getCurrentHP() > 0) {
-      System.out.println("You win!");
-    } else {
-      System.out.println("You lose...");
+    public void start() {
+        Scanner input = new Scanner(System.in);
+    
+        while (userPokemon.getCurrentHP() > 0 && computerPokemon.getCurrentHP() > 0) {
+            displayPokemonStatus();
+            int moveIndex = selectMove(input);
+            useMove(userPokemon, moveIndex);
+            if (computerPokemon.getCurrentHP() > 0) {
+                useMove(computerPokemon, 0);
+            }
+            System.out.println();
+        }
+
+        displayResult();
     }
-  }
+
+    private void displayPokemonStatus() {
+        System.out.println("Your Pokemon: " + userPokemon.getName());
+        System.out.println("HP: " + userPokemon.getCurrentHP() + "/" + userPokemon.getMaxHP());
+        System.out.println("Opponent's Pokemon: " + computerPokemon.getName());
+        System.out.println("HP: " + computerPokemon.getCurrentHP() + "/" + computerPokemon.getMaxHP());
+        System.out.println();
+    }
+
+    private int selectMove(Scanner input) {
+        System.out.println("Choose a move:");
+        for (int i = 0; i < userPokemon.getMoves().size(); i++) {
+            Move move = userPokemon.getMoves().get(i);
+            System.out.println((i + 1) + ": " + move.getName());
+        }
+        return input.nextInt() - 1;
+    }
+
+    private void useMove(Pokemon pokemon, int moveIndex) {
+        Move selectedMove = pokemon.getMoves().get(moveIndex);
+        pokemon.useMove(selectedMove, (pokemon == userPokemon) ? computerPokemon : userPokemon);
+        System.out.println();
+        System.out.println(pokemon.getName() + " used " + selectedMove.getName() + "!");
+    }
+
+    private void displayResult() {
+        if (userPokemon.getCurrentHP() > 0) {
+            System.out.println("You win!");
+        } else {
+            System.out.println("You lose...");
+        }
+    }
+
 }
